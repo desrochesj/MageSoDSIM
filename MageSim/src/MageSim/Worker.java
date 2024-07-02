f5c6c1de309bfb163dce36e89ea570f4d5764149
desrochesj <79223593+desrochesj@users.noreply.github.com>
2024-07-01 20:58:20 -0400

    Add files via upload

--- MageSim/src/MageSim/Worker.java
+++ MageSim/src/MageSim/Worker.java
@@ -1,6 +1,6 @@
 /*
- * Public Mage Sim SoD P3 (Sunken Temple)
- * 5/3/2024
+ * Public Mage Sim SoD
+ * 7/1/2024
  */
 
 package MageSim;
@@ -8,61 +8,249 @@ package MageSim;
 import java.io.*;
 import java.util.Scanner;
 
-public class Worker extends Player{
+public class Worker extends FFBDeepFire{
 	public static void main(String[] args) throws Exception
 	{
-		//Variables
+		//Timing Variables
 		int fightDuration = -1;
 		int remainingDuration = -1;
-		int damageDealt = 0;
+		int busyDuration = -1;
+		int ffbConnect = -1;
+		int igniteStart = -1;
+		int igniteEnd = -1;
+		int igniteTick = -1;
+		int bombExplode = -1;
+		int bombTick = -1;
+		boolean hasCrit = false;
+		boolean igniteActive = false;
+		boolean livingBomb = false;
 		
-		//Build Player
-		Player player = new Player();
+		//Damage Tracking Variables
+		int currentDamage = 0;
+		int currentFFB = 0;
+		int currentBombTick = 0;
+		int currentBombExplosion = 0;
+		int currentIgnite = 0;
+		int totalFFB = 0;
+		int totalBombTick = 0;
+		int totalBombExplosion = 0;
+		int totalIgnite = 0;
+		int totalDamage = 0;
+		
+		//Arrays
+		final int numIterations = 10000;
+		int[][] simValues = new int[5][numIterations];
+		
+		//Build Profile
+		FFBDeepFire profile = new FFBDeepFire();
 		
 		//Load Sim Characteristics
 		//TODO: manual txt file is JANK build automated solution
-		//TODO: build talent support instead of assuming builds
+		//TODO: build talent support instead of assuming build
 		try {
 			File simCharacteristics = new File(
 					"Sim Characteristics.txt");
 			Scanner fileReader = new Scanner(simCharacteristics);
 			while (fileReader.hasNextLine()) {
-				String locate = fileReader.nextLine(); //Fight Length
+				String locate = fileReader.nextLine(); //Fight Length [UNIT .1s]
 				fightDuration = Integer.parseInt(locate.substring(locate.lastIndexOf(":") + 1));
 				
 				locate = fileReader.nextLine(); //Mana
-				player.setRemainingMana(Integer.parseInt(locate.substring(locate.lastIndexOf(":") + 1)));
+				profile.setRemainingMana(Integer.parseInt(locate.substring(locate.lastIndexOf(":") + 1)));
 				
 				locate = fileReader.nextLine(); //Intellect
-				player.setIntellect(Integer.parseInt(locate.substring(locate.lastIndexOf(":") + 1)));
+				profile.setIntellect(Integer.parseInt(locate.substring(locate.lastIndexOf(":") + 1)));
 				
 				locate = fileReader.nextLine(); //Spellpower
-				player.setSpellPower(Integer.parseInt(locate.substring(locate.lastIndexOf(":") + 1)));
+				profile.setSpellPower(Integer.parseInt(locate.substring(locate.lastIndexOf(":") + 1)));
 				
 				locate = fileReader.nextLine(); //Crit Chance
-				player.setCritChance(Double.parseDouble(locate.substring(locate.lastIndexOf(":") + 1)));
+				profile.setCritChance(Double.parseDouble(locate.substring(locate.lastIndexOf(":") + 1)));
 				
 				locate = fileReader.nextLine(); //Crit Multiplier
-				player.setCritMultiplier(Double.parseDouble(locate.substring(locate.lastIndexOf(":") + 1)));
+				profile.setCritMultiplier(Double.parseDouble(locate.substring(locate.lastIndexOf(":") + 1)));
 				
 				locate = fileReader.nextLine(); //Hit Chance
-				player.setHitChance(Integer.parseInt(locate.substring(locate.lastIndexOf(":") + 1)));
+				profile.setHitChance(Integer.parseInt(locate.substring(locate.lastIndexOf(":") + 1)));
 			}
 			fileReader.close();
 		} catch (FileNotFoundException e){
-			System.out.println("Unable to located Sim Characteristics");
+			System.out.println("Unable to locate Sim Characteristics");
 			e.printStackTrace();
 		}
 		
 		//Run the Sim
-		//TODO iterate 10k times and take average
-		remainingDuration = fightDuration;
-		while (remainingDuration > 3) {
-			damageDealt += player.castFrostFireBolt();
-			remainingDuration -= 3;
+		for (int i = 0; i < numIterations; i++) {
+			//Reset Timing Variables
+			remainingDuration = fightDuration;
+			busyDuration = -1;
+			ffbConnect = -1;
+			igniteStart = -1;
+			igniteEnd = -1;
+			igniteTick = -1;
+			bombExplode = -1;
+			bombTick = -1;
+			hasCrit = false;
+			igniteActive = false;
+			livingBomb = false;
+			
+			//Reset Damage Variables
+			currentDamage = 0;
+			currentFFB = 0;
+			currentBombTick = 0;
+			currentBombExplosion = 0;
+			currentIgnite = 0;
+			totalFFB = 0;
+			totalBombTick = 0;
+			totalBombExplosion = 0;
+			totalIgnite = 0;
+			totalDamage = 0;
+			
+			while (remainingDuration > 0) {
+				//Spell Choice
+				//Living Bomb
+				if (busyDuration <= 0 && !livingBomb) {
+					//Spell Misses
+			    	if (profile.generateRandom(0,100) > 94 + profile.getHitChance()) {
+			    		//System.out.println("Living Bomb Missed");
+			    		busyDuration = 15;
+			    	} else {
+			    		bombExplode = remainingDuration - 120;
+						bombTick = remainingDuration - 30;
+						
+						//System.out.println("Living Bomb Applied");
+						//System.out.println("Time Left + Bomb Applied: " + remainingDuration);
+						livingBomb = true;
+						busyDuration = 15;
+			    	}
+				}
+				
+				//Frostfire Bolt
+				if (busyDuration <= 0) { 
+					ffbConnect = remainingDuration - 29;
+					//System.out.println("Time Left + FFB Cast Start: " + remainingDuration);
+					busyDuration = 30;
+				}
+				
+				//Direct Damage Effects
+				//FrostFire Bolt
+				if (remainingDuration == ffbConnect) {
+					if (profile.generateRandom(0,100) <= profile.getCritChance() * 100) {
+						hasCrit = true;
+					}
+					
+					currentFFB = profile.castFrostFireBolt(hasCrit);		
+					totalFFB += currentFFB;
+					currentDamage += currentFFB;
+				}
+				
+				//Living Bomb Explosion
+				if (remainingDuration == bombExplode) {
+					if (profile.generateRandom(0, 100) <= profile.getCritChance() * 100) {
+						hasCrit = true;
+					}
+					
+					currentBombExplosion = profile.castLivingBomb(hasCrit);
+					totalBombExplosion += currentBombExplosion;
+					currentDamage += currentBombExplosion;
+					
+					livingBomb = false;
+				}
+				
+				//Periodic Damage Effects
+				//Living Bomb Tick
+				if (remainingDuration == bombTick
+						|| remainingDuration == bombTick - 30
+						|| remainingDuration == bombTick - 60
+						|| remainingDuration == bombTick - 90) {
+					currentBombTick = profile.castLivingBombTick();
+					totalBombTick += currentBombTick;
+					currentDamage += currentBombTick;
+				}
+				
+				//Ignite
+				if (hasCrit) {
+					igniteStart = remainingDuration - 20;
+					igniteEnd = remainingDuration - 40;
+					
+					if (igniteActive) {
+						igniteTick += (int) Math.round(currentDamage * .2);
+					} else {
+						igniteTick = (int) Math.round(currentDamage * .2);
+						igniteActive = true;
+					}
+				}			
+				
+				//Apply Ignite Damage
+				if (remainingDuration == igniteStart) {
+					currentIgnite = igniteTick;
+					totalIgnite += currentIgnite;
+					currentDamage += currentIgnite;
+					
+					//System.out.println("Ignite did: " + currentDamage);
+				}
+				if (remainingDuration == igniteEnd) {
+					currentIgnite = igniteTick;
+					totalIgnite += currentIgnite;
+					currentDamage += currentIgnite;
+					
+					//System.out.println("Ignite did: " + currentDamage);
+					
+					igniteActive = false;
+				}
+				
+				//Increment Values and Reset States
+				totalDamage += currentDamage;
+				
+				currentDamage = 0;
+				hasCrit = false;
+				
+				busyDuration -= 1;
+				remainingDuration -= 1;
+			}
+			
+			//Add Sim Data to Array
+			simValues[0][i] = totalFFB;
+			simValues[1][i] = totalBombTick;
+			simValues[2][i] = totalBombExplosion;
+			simValues[3][i] = totalIgnite;
+			simValues[4][i] = totalDamage;
+			
+			//Print Out Single Sim Iteration
+			//System.out.println("");
+			//System.out.println("Damage Breakdown");
+			//System.out.println("Frostfire Bolt Damage        = " + simValues[0][i] + " | " + totalFFB * 10 / fightDuration);
+			//System.out.println("Living Bomb Tick Damage      = " + totalBombTick + "  | " + totalBombTick * 10 / fightDuration);
+			//System.out.println("Living Bomb Explosion Damage = " + totalBombExplosion + "  | " + totalBombExplosion * 10 / fightDuration);
+			//System.out.println("Ignite Damage                = " + totalIgnite + "  | " + totalIgnite * 10 / fightDuration);
+			//System.out.println("Total Damage Dealt           = " + totalDamage + " | " + totalDamage * 10 / fightDuration);
 		}
 		
-		System.out.println("Total Damage Dealt = " + damageDealt);
-		System.out.println("DPS = " + damageDealt / fightDuration);
+		//Reset All Totals
+		totalFFB = 0;
+		totalBombTick = 0;
+		totalBombExplosion = 0;
+		totalIgnite = 0;
+		totalDamage = 0;
+		
+		//Pull Overall Results from Array
+		for (int j = 0; j < numIterations; j++) {
+			totalFFB += simValues[0][j];
+			totalBombTick += simValues[1][j];
+			totalBombExplosion += simValues[2][j];
+			totalIgnite += simValues[3][j];
+			totalDamage += simValues[4][j];
+		}
+		
+		//Print Results After 10k Iterations
+		System.out.println("");
+		System.out.println("Damage Breakdown                DMG  | DPS");
+		System.out.println("__________________________________________");
+		System.out.println("");
+		System.out.println("Frostfire Bolt Damage        = " + totalFFB / numIterations + " | " + (totalFFB / numIterations) * 10 / fightDuration);
+		System.out.println("Living Bomb Tick Damage      = " + totalBombTick / numIterations + "  | " + (totalBombTick / numIterations) * 10 / fightDuration);
+		System.out.println("Living Bomb Explosion Damage = " + totalBombExplosion / numIterations + "  | " + (totalBombExplosion / numIterations) * 10 / fightDuration);
+		System.out.println("Ignite Damage                = " + totalIgnite / numIterations + "  | " + (totalIgnite / numIterations) * 10 / fightDuration);
+		System.out.println("Total Damage Dealt           = " + totalDamage / numIterations + " | " + (totalDamage / numIterations) * 10 / fightDuration);
 	}
 }
